dbd4c63516ac52b08f4432710a7d24c5594a4276
onursolmaz <solmaz_onur@hotmail.com>
2021-04-27 18:07:28 +0300

    added relation

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from news.models import Category
+from news.models import Category, News
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -9,4 +9,10 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ["status"]
 
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "status"]
+    list_filter = ["status"]
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(News, NewsAdmin)
--- news/models.py
+++ news/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
         ("True", "Evet"),
         ("False", "Hayır"),
     )
+
     title = models.CharField(max_length=60)
     keywords = models.CharField(max_length=150)
     description = models.CharField(max_length=200)
@@ -15,10 +16,27 @@ class Category(models.Model):
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
     parent = models.ForeignKey("self", blank=True, null=True, related_name="children", on_delete=models.CASCADE)
-    create_at=models.DateField(auto_now_add=True)
-    update_at=models.DateField(auto_now=True)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
 
 
+class News(models.Model):
+    STATUS = (
+        ("True", "Evet"),
+        ("False", "Hayır"),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with Category
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=70)
+    image = models.ImageField(blank=True, upload_to="images/")
+    status = models.CharField(max_length=10, choices=STATUS)
+    detail = models.TextField()
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
