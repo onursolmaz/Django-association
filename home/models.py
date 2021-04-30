1c1be09dbd4e0c2a6b9584204a98579ae68fa0bc
onursolmaz <solmaz_onur@hotmail.com>
2021-04-30 03:29:29 +0300

    added dynamic settings

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Setting(models.Model):
+    title = models.CharField(max_length=75, blank=True)
+    keywords = models.CharField(max_length=200)
+    description = models.CharField(max_length=255)
+    address = models.CharField(blank=True, max_length=150)
+    phone = models.CharField(blank=True, max_length=15)
+    email = models.CharField(blank=True, max_length=30)
+    email = models.CharField(blank=True, max_length=30)
+    aboutus = models.TextField()
+    smtpserver = models.CharField(blank=True, max_length=16)
+    smtpemail = models.CharField(blank=True, max_length=30)
+    smtppassword = models.CharField(blank=True, max_length=15)
+    smtpport = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to="images/")
+    facebook = models.CharField(blank=True, max_length=100)
+    instagram = models.CharField(blank=True, max_length=100)
+    linkedin = models.CharField(blank=True, max_length=100)
+    youtube = models.CharField(blank=True, max_length=100)
+
+    def __str__(self):
+        return self.title
